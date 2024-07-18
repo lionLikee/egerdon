a6772c56839e7239610909fc2bc4f3932302924b
이제원 <lys28750@naver.com>
2024-07-18 19:40:09 +0900

    design

--- src/components/Commerse.jsx
+++ src/components/Commerse.jsx
@@ -1,6 +1,23 @@
 
+import commerse_pic1 from "./commerse1.png";
+import commerse_pic2 from "./commerse2.png";
+import commerse_pic3 from "./commerse3.png";
+import "./commerse.css"
+
+
 export function Commerse(){
     return(
-        <p>커머스</p>
+        <div className="picBox">
+            <a href="https://smartstore.naver.com/royalcanin/category/ace2fe8316bb42489b6fe18c108b94cd?cp=1&NaPm=ct%3Dlyqy0d5c|ci%3D0zq0001UKDvA9mx501nH|tr%3Dsa|hk%3D58933b4707bb22e1223f6f6af0553daf3ccd3db7|nacn%3Dc6SKBMg1dilZ" target="_blank" rel="noopener noreferrer">
+                <img src={commerse_pic1} className="commerse_pics" alt="Commerse 1" />
+            </a>
+            <a href="https://www.kebhana.com/cont/mall/mall08/mall0801/mall080102/1470991_115157.jsp" target="_blank" rel="noopener noreferrer">
+                <img src={commerse_pic2} className="commerse_pics" alt="Commerse 2" />
+            </a>
+            <a href="https://www.kbinsure.co.kr/CG313010001.ec" target="_blank" rel="noopener noreferrer">
+                <img src={commerse_pic3}  className="commerse_pics" alt="Commerse 3" />
+            </a>
+        </div>
+        
     );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import {useNavigate} from "react-router-dom";
+import "./home.css";
+import logo from "./seviceLogo.png";
+import Btn from  "./orangeBtn.png";
+import pic1 from "./pic1.png";
+import pic2 from "./pic2.png";
 
 
 export function Home(){
@@ -7,9 +12,24 @@ export function Home(){
 
     return(
         <div className="homepage">
-            <button className="joinBtn" onClick={()=>navigate("/join")}>회원가입</button>
-            <button className="loginBtn" onClick={()=>navigate("/login")}>로그인</button>
-            <button className="commerseBtn" onClick={()=>navigate("/commerse")}>commerse</button>
+            <div className="navBar">
+                <img src={logo} alt="logo" className="logo_pic"></img>
+                <button className="joinBtn" onClick={()=>navigate("/join")}>회원가입</button>
+                <button className="loginBtn" onClick={()=>navigate("/login")}>로그인</button>
+                <button className="commerseBtn" onClick={()=>navigate("/commerse")}>COMMERSE</button>
+            </div>
+
+            <div className="homeText">
+                <h1>펫을 위한 완벽한 가계부.</h1>
+                <h3 className="text2">당신의 소중한 펫을 위한 모든 소비를 한눈에 정리하고</h3>
+                <h3 className="text3">빠르게 관리할 수 있어요</h3>
+            </div>
+
+            <img src={Btn} className="anotherBtn" onClick={()=>navigate("/login")}></img>
+            <div className="pictures">
+                <img src={pic1} className="pic1"></img>
+                <img src={pic2} className="pic2"></img>
+            </div>
         </div>
     );
 }
