afec37430a1a355622cc22f1116ca11f9f31dbce
이제원 <lys28750@naver.com>
2024-07-18 18:18:39 +0900

    2차 수정

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import { Loginpage } from "./components/Loginpage";
 import { Joinpage } from "./components/Joinpage";
 import { Community } from "./components/Community";
 import { Commerse } from "./components/Commerse";
-import { Landing } from "./components/Landing";
 import { PetList } from "./components/PetList";
 import { PetDetail } from "./components/PetDetail";
 
 function App() {
   return (
   <Routes>
-    <Route path="/" element={<Landing />}/>
-    <Route path="/home" element={<Home/>}/>
-    <Route path="/home/join" element={<Joinpage/>}/>
-    <Route path="/home/login" element={<Loginpage/>}/>
-    <Route path="/home/community" element={<Community/>}/>
-    <Route path="/home/commerse" element={<Commerse/>}/>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/join" element={<Joinpage/>}/>
+    <Route path="/login" element={<Loginpage/>}/>
+    <Route path="/community" element={<Community/>}/>
+    <Route path="/commerse" element={<Commerse/>}/>
     <Route path="/mypage" element={<PetList/>}/>
     <Route path="/mypage/petdetail" element={<PetDetail />} />
     
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,9 @@ export function Home(){
 
     return(
         <div className="homepage">
-            <button className="joinBtn" onClick={()=>navigate("/home/join")}>회원가입</button>
-            <button className="loginBtn" onClick={()=>navigate("/home/login")}>로그인</button>
-            <button className="commerseBtn" onClick={()=>navigate("/home/commerse")}>commerse</button>
+            <button className="joinBtn" onClick={()=>navigate("/join")}>회원가입</button>
+            <button className="loginBtn" onClick={()=>navigate("/login")}>로그인</button>
+            <button className="commerseBtn" onClick={()=>navigate("/commerse")}>commerse</button>
         </div>
     );
 }
